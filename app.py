d530923a92da25e817dec113f2f8eb68d5d0204b
Yubeen Huh <casshusky@gmail.com>
2020-08-27 21:49:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ def login():
 def register():
    return render_template('register_try1.html')
 
+@app.route('/myPage')
+def myPage():
+   return render_template('my_page.html')
+
 #################################
 ##  로그인을 위한 API            ##
 #################################
@@ -54,6 +58,28 @@ def api_register():
    db.user.insert_one({'id':id_receive,'pw':pw_hash, 'nickname':nickname_receive, 'sex':sex_receive, 'age':age_receive})
 
    return jsonify({'result': 'success'})
+
+# 시 저장하기
+@app.route('/api/myPage', methods=['POST'])
+def api_myPage():
+   poemTitle_receive = request.form['poemTitle_give']
+   poem_receive = request.form['poem_give']
+   token_receive = request.headers['token_give']
+   payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+   userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
+
+   db.poem.insert_one({'poemTitle':poemTitle_receive,'poem':poem_receive, 'id':userinfo['id']})
+
+   return jsonify({'result': 'success'})
+
+@app.route('/api/myPage', methods=['GET'])
+def api_get_myPage():
+   token_receive = request.headers['token_give']
+   payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+   userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
+   poems = list(db.poem.find({'id':payload['id']},{'_id':0}))
+
+   return jsonify({'result': 'success', 'poems': poems})
 
 # [로그인 API]
 # id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
@@ -76,7 +102,7 @@ def api_login():
       # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
       payload = {
          'id': id_receive,
-         'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30)
+         'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=2)
       }
       token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
 
@@ -86,29 +112,16 @@ def api_login():
    else:
       return jsonify({'result': 'fail', 'msg':'아이디/비밀번호가 일치하지 않습니다.'})
 
-# [유저 정보 확인 API]
-# 로그인된 유저만 call 할 수 있는 API입니다.
-# 유효한 토큰을 줘야 올바른 결과를 얻어갈 수 있습니다.
-# (그렇지 않으면 남의 장바구니라든가, 정보를 누구나 볼 수 있겠죠?)
 @app.route('/api/nick', methods=['GET'])
 def api_valid():
-   # 토큰을 주고 받을 때는, 주로 header에 저장해서 넘겨주는 경우가 많습니다.
-   # header로 넘겨주는 경우, 아래와 같이 받을 수 있습니다.
    token_receive = request.headers['token_give']
 
-   # try / catch 문?
-   # try 아래를 실행했다가, 에러가 있으면 except 구분으로 가란 얘기입니다.
-
    try:
-      # token을 시크릿키로 디코딩합니다.
-      # 보실 수 있도록 payload를 print 해두었습니다. 우리가 로그인 시 넣은 그 payload와 같은 것이 나옵니다.
       payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
       print(payload)
 
-      # payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
-      # 여기에선 그 예로 닉네임을 보내주겠습니다.
       userinfo = db.user.find_one({'id':payload['id']},{'_id':0})
-      return jsonify({'result': 'success','nickname':userinfo['nick']})
+      return jsonify({'result': 'success','nickname':userinfo['nickname']})
    except jwt.ExpiredSignatureError:
       # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
       return jsonify({'result': 'fail', 'msg':'로그인 시간이 만료되었습니다.'})
